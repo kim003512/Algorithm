28cad0636d63c56afbfb43e408055a3a55e8cc48
min_jjiny <kim003512@naver.com>
2020-05-17 13:15:27 +0900

    정수삼각형 완료

--- algorithm_prac/src/dp/algo_1932_dp.java
+++ algorithm_prac/src/dp/algo_1932_dp.java
@@ -5,31 +5,6 @@ import java.util.*;
 public class algo_1932_dp {
 	static int[][] list;
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		
-//		int n = sc.nextInt();
-//		
-//		int[][] tri = new int[n+1][n+1];
-//		
-//		int tmp=0, sum=0;
-//		
-//		for(int i = 1; i<=n; i++) {
-//			for(int j=1; j<=i; j++) {
-//				tri[i][j] = sc.nextInt();
-//				
-//				if(j==1)
-//					//
-//				else if(j==i)
-//				
-//				
-//				
-//			
-//			
-//			}
-//		}
-	
-		 
-	 
 	        Scanner sc = new Scanner(System.in);
 	        int n = sc.nextInt();
 	        int sum = 0, tmp = 0;
@@ -51,10 +26,5 @@ public class algo_1932_dp {
 	            }
 	        }
 	        System.out.println(sum);
-	 
 	    }
-
-
-	
-
 }
