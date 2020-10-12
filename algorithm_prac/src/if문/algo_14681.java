26037277a196b5e1da42a058bbb9ca61b299e2f6
KMJ <kim003512@naver.com>
2020-10-12 20:52:46 +0900

    algo_14681

---
+++ algorithm_prac/src/if문/algo_14681.java
@@ -0,0 +1,21 @@
+package if¹®;
+
+import java.util.Scanner;
+
+public class algo_14681 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		if(a > 0) {
+			if(b>0) System.out.println("1");
+			else if(b<0) System.out.println("4");
+		} else {
+			if(b>0) System.out.println("2");
+			else if(b<0) System.out.println("3");
+		}
+	}
+}
--- algorithm_prac/src/if문/algo_2753.java
+++ algorithm_prac/src/if문/algo_2753.java
@@ -1,10 +1,20 @@
 package if¹®;
 
+import java.util.Scanner;
+
 public class algo_2753 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		
+		int year = s.nextInt();
+		
+		if(year % 4 ==0) {
+			if(year%400 == 0) System.out.println("1");
+			else if (year%100==0) System.out.println("0");
+			else System.out.println("1");
+		}
+		else System.out.println("0");
 	}
 
 }
